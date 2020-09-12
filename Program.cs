bc97568ed97e4d1bd09d555c7a6274d37eb72fe2
Ilya Budnik <ilyabudnikxs@mail.ru>
2020-09-12 17:49:02 +0300

    Start v1.0

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер задания/функции:");
+            int num_switch = Convert.ToInt32(Console.ReadLine());
+            switch(num_switch)
+            {
+                case 1:
+                    func1(args);  
+                    break;
+            }
+        }
+        static void func1(string[] args)
+        {
+            Console.WriteLine("sosi");
+        }
+    }
+}
